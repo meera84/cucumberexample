8a7ef938ed12d08862d9f9ccb4031e156f5e5b59
meera84 <meera.lokaswaran@gmail.com>
2021-03-09 08:39:24 +0800

    tom spring

---
+++ spring-cucumber/src/test/java/com/example/demo/CucumberTest.java
@@ -0,0 +1,20 @@
+package com.example.demo;
+
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
+import io.cucumber.spring.CucumberContextConfiguration;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@CucumberContextConfiguration
+@RunWith(Cucumber.class)
+@CucumberOptions(
+        plugin = {"pretty", "junit:build/cucumber-junit.xml", "html:build/cucumber.html"},
+        features = "classpath:/"
+)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@AutoConfigureMockMvc
+public class CucumberTest {
+
+}
---
+++ spring-cucumber/src/test/java/com/example/demo/SomeSteps.java
@@ -0,0 +1,34 @@
+package com.example.demo;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.junit.platform.engine.Cucumber;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@Cucumber
+public class SomeSteps {
+
+    private String today;
+    private String actualAnswer;
+
+    @Given("today is {string}")
+    public void today_is(String today) {
+        this.today = today;
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        actualAnswer = isItFriday(today);
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String expectedAnswer) {
+        assertEquals(expectedAnswer, actualAnswer);
+    }
+
+    private static String isItFriday(String today) {
+        return "Friday".equals(today) ? "TGIF" : "Nope";
+    }
+}
